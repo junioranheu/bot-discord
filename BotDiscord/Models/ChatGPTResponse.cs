ef3e8d52319f6fc7f570b4ca0790383a3880a376
junioranheu <junioranheu@gmail.com>
2023-04-03 00:28:18 -0300

    iniciar refactor em ChatGPTCommands

--- BotDiscord/Models/ChatGPTResponse.cs
+++ BotDiscord/Models/ChatGPTResponse.cs
@@ -1,7 +1,20 @@
-﻿namespace BotDiscord.Models
+﻿using OpenAI.GPT3.ObjectModels.ResponseModels;
+using OpenAI.GPT3.ObjectModels.SharedModels;
+
+namespace BotDiscord.Models
 {
     public sealed class ChatGPTResponse
     {
-        public string[]? Choices { get; set; }
+        public List<ChoiceResponse>? Choices { get; set; }
+
+        public int CreatedAt { get; set; }
+
+        public Error? Error { get; set; }
+
+        public string Id { get; set; } = string.Empty;
+
+        public string Model { get; set; } = string.Empty;
+
+        public bool Successful { get; set; } = false;
     }
 }
--- BotDiscord/Modules/ChatGPTCommands.cs
+++ BotDiscord/Modules/ChatGPTCommands.cs
@@ -1,4 +1,5 @@
 ﻿using BotDiscord.Common;
+using BotDiscord.Models;
 using Discord;
 using Discord.Commands;
 using OpenAI.GPT3;
@@ -13,31 +14,56 @@ namespace BotDiscord.Modules
         [Command("ctm", RunMode = RunMode.Async)]
         public async Task ChatGPT([Remainder] string? texto)
         {
-            OpenAIService gpt3 = new(new OpenAiOptions()
-            {
-                ApiKey = "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"
-            });
+            CompletionCreateResponse respChatGPT = await ObterCompletionCreateResponse(texto);
+            ChatGPTResponse chatGPTResponse = ObterChatGPTResponse(respChatGPT);
 
-            CompletionCreateResponse resultado = await gpt3.Completions.CreateCompletion(new CompletionCreateRequest()
+            if (!chatGPTResponse.Successful)
             {
-                Prompt = texto,
-                Model = OpenAI.GPT3.ObjectModels.Models.TextDavinciV2,
-                Temperature = 0.5F,
-                MaxTokens = 100,
-                N = 3
-            });
+                await Logger.Log(LogSeverity.Critical, $"{nameof(ChatGPTCommands)}", chatGPTResponse?.Error?.Message ?? string.Empty);
 
-            if (!resultado.Successful)
-            {
-                await Logger.Log(LogSeverity.Info, $"{nameof(ChatGPTCommands)}", resultado?.Error?.Message ?? string.Empty);
-                string erro = !string.IsNullOrEmpty(resultado?.Error?.Message) ? $": {resultado.Error.Message}" : string.Empty;
+                string erro = !string.IsNullOrEmpty(chatGPTResponse?.Error?.Message) ? $": {chatGPTResponse.Error.Message}" : string.Empty;
                 await Context.Message.ReplyAsync($"Ops, {Context.User.Username}! Parece que houve um erro{erro}");
+                return;
             }
 
-            foreach (var choice in resultado!.Choices)
+            foreach (var choice in chatGPTResponse!.Choices)
             {
                 //await Context.Message.ReplyAsync(choice.Text);
             }
+        }
+
+        private static async Task<CompletionCreateResponse> ObterCompletionCreateResponse(string? texto)
+        {
+            OpenAIService gpt3 = new(new OpenAiOptions()
+            {
+                ApiKey = apiKey
+            });
+
+            CompletionCreateResponse respChatGPT = await gpt3.Completions.CreateCompletion(new CompletionCreateRequest()
+            {
+                Prompt = texto ?? string.Empty,
+                Model = OpenAI.GPT3.ObjectModels.Models.TextDavinciV2,
+                Temperature = 0.5F,
+                MaxTokens = 100,
+                N = 1
+            });
+
+            return respChatGPT;
+        }
+
+        private static ChatGPTResponse ObterChatGPTResponse(CompletionCreateResponse respChatGPT)
+        {
+            ChatGPTResponse response = new()
+            {
+                Choices = respChatGPT.Choices,
+                CreatedAt = respChatGPT.CreatedAt,
+                Error = respChatGPT.Error,
+                Id = respChatGPT.Id,
+                Model = respChatGPT.Model,
+                Successful = respChatGPT.Successful
+            };
+
+            return response;
         }
     }
 }
