dd0a0af2bae2b07d102b09cd10f1270a5c7a581c
junioranheu <junioranheu@gmail.com>
2023-04-02 23:10:56 -0300

    implementar Utils

---
+++ BotDiscord/Common/Utils.cs
@@ -0,0 +1,13 @@
+﻿using TimeZoneConverter;
+
+namespace BotDiscord.Common
+{
+    public static class Utils
+    {
+        public static DateTime HorarioBrasilia()
+        {
+            TimeZoneInfo timeZone = TZConvert.GetTimeZoneInfo("E. South America Standard Time");
+            return TimeZoneInfo.ConvertTime(DateTime.UtcNow, timeZone);
+        }
+    }
+}
